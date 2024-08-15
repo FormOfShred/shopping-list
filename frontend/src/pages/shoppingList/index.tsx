377c5a42931b322c2ffbc58c7fbd135c62362dfd
Robyn <r0797966@ucll.be>
2024-08-15 10:15:40 +0200

    open shopping list page

--- frontend/src/pages/shoppingList/index.tsx
+++ frontend/src/pages/shoppingList/index.tsx
@@ -1,8 +1,19 @@
+import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, Key } from "react";
+import { useLocation } from "react-router-dom";
+
 const ShoppingList = () => {
+  const location = useLocation();
+  const { title, items } = location.state || { title: '', items: [] };
+
   return (
     <div>
-      <h1>Shopping List</h1>
-    </div>
+      <p>{ title }</p>
+      <ul>
+          { items.map((item: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined, index: Key | null | undefined) => {
+              return <li key={index}>{ item }</li>
+          }) }
+      </ul>
+  </div>
   );
 };
 
