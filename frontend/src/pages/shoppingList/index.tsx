dc64967c0489dd97dce15206298dd563afdefce0
Robyn <r0797966@ucll.be>
2024-08-15 11:00:48 +0200

    check items

--- frontend/src/pages/shoppingList/index.tsx
+++ frontend/src/pages/shoppingList/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ShoppingList: React.FC = () => {
   const location = useLocation();
   const { title, items } = location.state || { title: '', items: [] };
 
+  const [ checkedItems, setCheckedItems ] = useState<number[]>([]);
+
   const handleItemClick = (index: number) => {
     console.log('Item clicked', items[index]);
+    checkedItems.includes(index) ? setCheckedItems(checkedItems.filter(item => item !== index)) : setCheckedItems([...checkedItems, index]);
   };
 
   return (
@@ -14,7 +17,9 @@ const ShoppingList: React.FC = () => {
       <p>{title}</p>
       <ul>
         {items.map((item: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined, index: number) => (
-          <li onClick={() => handleItemClick(index)} key={index}>
+          <li onClick={() => handleItemClick(index)} key={index}
+            style={{ textDecoration: checkedItems.includes(index) ? 'line-through' : 'none', cursor: 'pointer' }}
+            >
             {item}
           </li>
         ))}
