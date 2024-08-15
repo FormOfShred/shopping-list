444477064cc7b4b5e3f8bf8c3b743ae95ea3dac8
Robyn <r0797966@ucll.be>
2024-08-15 09:39:12 +0200

    create list component

---
+++ frontend/src/components/Dashboard/list.tsx
@@ -0,0 +1,14 @@
+const List = () => {
+    return (
+        <div>
+        {/* List 1 */}
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+        </ul>
+      </div>
+    );
+};
+
+export default List;
---
+++ frontend/src/pages/shoppingList/index.tsx
@@ -0,0 +1,7 @@
+export default function ShoppingList() {
+    return (
+      <div>
+        <p>Shopping List</p>
+      </div>
+    );
+  }
