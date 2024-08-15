d2f621b4d2fd2396d6bef8dd256e25cc2464bd4e
Robyn <r0797966@ucll.be>
2024-08-15 09:47:51 +0200

    loop through static lists

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,10 +6,31 @@ import List from './components/Dashboard/list';
 
 
 function App() {
+  {/* Lists */}
+
+  const firstList = {
+    title: 'Food',
+    items: ['Apple', 'Banana', 'Orange']
+  }
+
+  const secondList = {
+    title: 'Toys',
+    items: ['Car', 'Truck', 'Airplane']
+  }
+
+  const thirdList = {
+    title: 'Books',
+    items: ['Book 1', 'Book 2', 'Book 3']
+  }
+
+  const lists = [firstList, secondList, thirdList];
+
   return (
       <div className="App">
         <p>Shopping List</p>
-        <List />
+        {lists.map((list, index) => (
+          <List key={index} title={list.title} items={list.items} />
+        ))}
       </div>
   );
 }
--- frontend/src/components/Dashboard/list.tsx
+++ frontend/src/components/Dashboard/list.tsx
@@ -1,13 +1,20 @@
-const List = () => {
+import React from 'react';
+
+const List = (props: { title: string, items: string[] }) => {
+    const { title, items } = props;
+
+    console.log(title, items);
+
     return (
         <div>
-        {/* List 1 */}
-        <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-        </ul>
-      </div>
+            {/* List 1 */}
+            <p>{ title }</p>
+            <ul>
+                { items.map((item, index) => {
+                    return <li key={index}>{ item }</li>
+                }) }
+            </ul>
+        </div>
     );
 };
 
